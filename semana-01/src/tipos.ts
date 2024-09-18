0df58c7a0b898b016ce6d8803777f2311c9cdd84
laerciosouzaDev <laerciogplay@gmail.com>
2024-09-17 21:42:01 -0300

    interfaces

--- semana-01/src/tipos.ts
+++ semana-01/src/tipos.ts
@@ -9,15 +9,13 @@ type Categoria = {
 
 
 var nome = leia.question("informe o nome da categoria: ")
-var info = leia.question("escolha o pc: ")
-var bolsa = leia.question("escolha o bolsa: ")
-var eletro = leia.question("escolha o eletro: ")
+var descricao = leia.question("descrição: ")
+
 
 var categoria: Categoria ={
     nome: nome,
-    informatica: info,
-    bolsas: bolsa,
-    eletro: eletro
+    
+    
 
 }
 
